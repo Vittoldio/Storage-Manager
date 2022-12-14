8453d1dab064b31946320df98b10cf7a546002a8
MetaphysicsNecrosis <MetaphysicsNecrosis@DESKTOP-A9QI9P7>
2022-12-14 19:43:37 +0300

    create start layout

---
+++ StorageManager/MainForm.cs
@@ -0,0 +1,11 @@
+namespace StorageManager
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            // Здесь будут все уведомления для юзера/админа + панель доступа в другие формы 
+            InitializeComponent();
+        }
+    }
+}
