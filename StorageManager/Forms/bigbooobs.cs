17f5be098d79760bc6ca9d0cc71ac62413a26e4a
MetaphysicsNecrosis <MetaphysicsNecrosis@DESKTOP-A9QI9P7>
2022-12-18 19:19:12 +0300

    pushme

--- StorageManager/Forms/bigbooobs.cs
+++ StorageManager/Forms/bigbooobs.cs
@@ -1,4 +1,5 @@
 ﻿using StorageManager.Data;
+using StorageManager.Enumirations;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,14 +19,25 @@ namespace StorageManager.Forms
             InitializeComponent();
             UpdateComboBoxes();
         }
-        DB_plug_task dB_Plug_Task = new DB_plug_task();
+        //DB_plug_task dB_Plug_Task = new DB_plug_task();
+        DB_plug_user users = new DB_plug_user();
         private void UpdateComboBoxes()
         {
-            for(int i = 0; i < dB_Plug_Task.gotTasks.Count; i++)
+            /*for(int i = 0; i < dB_Plug_Task.gotTasks.Count; i++)
             {
                 comboBox2.Items.Add(dB_Plug_Task.gotTasks[i].TaskPerfomer);
                 comboBox3.Items.Add(dB_Plug_Task.gotTasks[i].TaskType);
+            }*/
+            for(int i = 0;i < users.login.Count; i++)
+            {
+                if (users.login[i] != null) 
+                {
+                    comboBox2.Items.Add(users.login[i]);
+                }
             }
+            comboBox3.Items.Add(TaskType.sell);
+            comboBox3.Items.Add(TaskType.relocation);
+            comboBox3.Items.Add(TaskType.remove);
             textBox2.Text = Program.login;
         }
     }
